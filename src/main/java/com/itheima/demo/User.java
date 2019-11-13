2fe4d54067c70291a237b46d24764f9b48c5ba93
xiaoyue <1776563692@qq.com>
2019-11-13 17:55:18 +0800

    firstcommit

---
+++ src/main/java/com/itheima/demo/User.java
@@ -0,0 +1,15 @@
+package com.itheima.demo;
+
+/**
+ * @BelongsProject: git_demo
+ * @BelongsPackage: com.itheima.demo
+ * @Author: 86189
+ * @CreateTime: 2019-11-13 17:42
+ * @Description:
+ * @name:xiaoyue
+ */
+public class User {
+    public static void main(String[] args) {
+        System.out.println("成功上传");
+    }
+}
